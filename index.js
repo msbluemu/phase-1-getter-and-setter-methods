bbc1911b17d3f4f56ff162e23ee6f01ac0c17b36
Qianyun Mu <msbluemu@gmail.com>
2023-06-08 17:02:31 +1000

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Add your Circle class here
+class Circle {
+    constructor(radius){
+        this.radius = radius;
+    }
+
+    get diameter(){
+        return this.radius *2
+    }
+
+    get circumference(){
+        return this.diameter * Math.PI
+    }
+
+    get area(){
+        return this.radius*this.radius * Math.PI
+    }
+
+    set diameter(value){
+        this.radius = value/2
+    }
+
+    set circumference(value){
+        this.radius = (value/(Math.PI))/2
+    }
+
+    set area(value){
+        this.radius = Math.sqrt(value/Math.PI)
+    }
+}
+
+
+const cir1 = new Circle(5);
+console.log(cir1.diameter);
+console.log(cir1.circumference);
+console.log(cir1.area);
+
+cir1.diameter = 20;
+// cir1.circumference = 20;
+// cir1.area = 100;
+console.log(cir1.radius)
